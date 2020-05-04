renesas-rz/meta-renesas-ai

commit a734dc76e32833e53250457b1e97cf54d9824b78

pytorch: Add PyTorch v1.4.1 support
Signed-off-by: Christopher Feghali <christopher.feghali.xb@renesas.com>

meta-pytorch/recipes-devtools/torchvision/torchvision_0.5.0.bb
@@ -1,0 +1,34 @@
+SUMMARY = "Torchvision"
+DESCRIPTION = "Package library for computer vision"
+HOMEPAGE = "https://pytorch.org/"
+LICENSE = "MIT"
+
+LIC_FILES_CHKSUM = "file://LICENSE;md5=bd7749a3307486a4d4bfefbc81c8b796"
+
+S = "${WORKDIR}/git"
+
+SRC_URI = "git://github.com/pytorch/vision.git;protocol=git"
+SRCREV = "3bd7a405093742e837b2dc48d3c83fc16614d5df"
+
+COMPATIBLE_MACHINE = "(x86_64|iwg20m-g1m|iwg21m|iwg22m|hihope-rzg2m|ek874)"
+
+inherit pythonnative setuptools
+
+DEPENDS += " \
+	python \
+	pytorch-native \
+	python-future-native \
+"
+RDEPENDS_${PN} = " \
+	tiff \
+	pytorch \
+	python-numpy \
+	python-pillow \
+	python-six \
+"
+
+do_compile() {
+	export NATIVE_BUILD_DIR=${RECIPE_SYSROOT_NATIVE}
+	export CMAKE_PREFIX_PATH=${RECIPE_SYSROOT}/${prefix}
+	python ${S}/setup.py build
+}

meta-pytorch/recipes-mathematics/pytorch/pytorch_1.4.1.bb
@@ -1,0 +1,72 @@
+SUMMARY = "PyTorch Framework"
+DESCRIPTION = "AI Framework PyTorch"
+HOMEPAGE = "https://pytorch.org/"
+LICENSE = "BSD-3-Clause"
+
+LIC_FILES_CHKSUM = "file://LICENSE;md5=acf4d595f99e159bf31797aa872aef57"
+
+S = "${WORKDIR}/git"
+
+SRC_URI = " \
+	gitsm://github.com/pytorch/pytorch.git;protocol=git;nobranch=1 \
+	file://0001-add-base-cmake-configuration.patch \
+"
+
+SRCREV = "74044638f755cd8667bedc73da4dbda4aa64c948"
+
+SRC_URI_append_arm = " \
+	file://0002-add-armv7l-processor-into-check-condition-cpuinfo.patch \
+	file://0003-add-cmake-configuration-for-arm.patch \
+"
+
+SRC_URI_append_aarch64 = " \
+	file://0002-add-cmake-configuration-for-aarch64.patch \
+"
+COMPATIBLE_MACHINE = "(x86_64|iwg20m-g1m|iwg21m|iwg22m|hihope-rzg2m|ek874)"
+
+inherit pythonnative setuptools
+
+DEPENDS += " \
+	protobuf \
+	protobuf-native \
+	sleef-native \
+	python-numpy-native \
+	python-pyyaml-native \
+	python-typing-native \
+	glog \
+"
+
+RDEPENDS_${PN} = " \
+	python-modules \
+	python-future \
+	python-numpy \
+	gflags \
+	glog \
+"
+
+TARGET_CC_ARCH += "${LDFLAGS}"
+
+CFLAGS_append_arm = " -mfp16-format=ieee "
+CXXFLAGS_append_arm = " -mfp16-format=ieee "
+
+do_compile() {
+	export USE_CUDA=OFF
+	export USE_NUMA=OFF
+	export USE_MKLDNN=OFF
+	export USE_QNNPACK=ON
+	export USE_NNPACK=ON
+	export USE_FBGEMM=OFF
+	export BUILD_CUSTOM_PROTOBUF=OFF
+	export BUILD_CAFFE2_OPS=OFF
+	export BUILD_PYTHON=ON
+	export BUILD_TEST=ON
+	export CMAKE_SYSROOT=${RECIPE_SYSROOT}
+	python ${S}/setup.py build
+}
+
+do_install_append() {
+	install ${D}/${PYTHON_SITEPACKAGES_DIR}/torch/lib/*.so ${D}/${libdir}
+}
+
+PACKAGES = "libtorch pytorch-dbg pytorch" 
+FILES_libtorch = "${libdir}/*.so" 

meta-pytorch/COPYING.MIT
@@ -1,0 +1,17 @@
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.

meta-pytorch/conf/layer.conf
@@ -1,0 +1,15 @@
+# We have a conf and classes directory, add to BBPATH
+BBPATH .= ":${LAYERDIR}"
+
+# We have recipes-* directories, add to BBFILES
+BBFILES += "${LAYERDIR}/recipes-*/*/*.bb \
+	${LAYERDIR}/recipes-*/*/*.bbappend"
+
+BBFILE_COLLECTIONS += "pytorch"
+BBFILE_PATTERN_pytorch= "^${LAYERDIR}/"
+BBFILE_PRIORITY_pytorch= "6"
+
+PREFERRED_VERSION_protobuf-native ?= "3.6.0%"
+PREFERRED_VERSION_protobuf ?= "3.6.0%"
+
+META_PYTORCH_DIR = "${LAYERDIR}"

meta-pytorch/recipes-devtools/cmake/cmake-native/0001-FortranCInterface-Fix-broken-search-for-test-exe-whe.patch
@@ -1,0 +1,53 @@
+From 0fc7b9df1c3b6515395bf646deffb25e91cb415b Mon Sep 17 00:00:00 2001
+From: Brad King <brad.king@kitware.com>
+Date: Thu, 27 Feb 2020 11:04:20 -0500
+Subject: [PATCH] FortranCInterface: Fix broken search for test exe when
+ cross-compiling
+
+Previously the `find_program` call we used to locate the test executable
+but that can be broken by `CMAKE_FIND_ROOT_PATH_MODE_PROGRAM`.  Instead
+teach the test project to write a file with the location of the
+executable it builds.  Load that file to get the exact location.
+
+Fixes: #20390
+---
+ Modules/FortranCInterface/CMakeLists.txt |  4 ++++
+ Modules/FortranCInterface/Detect.cmake   | 10 ++--------
+ 2 files changed, 6 insertions(+), 8 deletions(-)
+
+diff --git a/Modules/FortranCInterface/CMakeLists.txt b/Modules/FortranCInterface/CMakeLists.txt
+index e3b81d7..71bd3ad 100644
+--- a/Modules/FortranCInterface/CMakeLists.txt
++++ b/Modules/FortranCInterface/CMakeLists.txt
+@@ -102,3 +102,7 @@ set_property(TARGET symbols PROPERTY POSITION_INDEPENDENT_CODE 1)
+ # Require symbols through Fortran.
+ add_executable(FortranCInterface main.F call_sub.f ${call_mod})
+ target_link_libraries(FortranCInterface PUBLIC symbols)
++
++file(GENERATE OUTPUT exe-$<CONFIG>.cmake CONTENT [[
++set(FortranCInterface_EXE "$<TARGET_FILE:FortranCInterface>")
++]])
+diff --git a/Modules/FortranCInterface/Detect.cmake b/Modules/FortranCInterface/Detect.cmake
+index 7789785..1431dd8 100644
+--- a/Modules/FortranCInterface/Detect.cmake
++++ b/Modules/FortranCInterface/Detect.cmake
+@@ -43,17 +43,11 @@ set(FortranCInterface_COMPILED ${FortranCInterface_COMPILED})
+ unset(FortranCInterface_COMPILED CACHE)
+ 
+ # Locate the sample project executable.
++set(FortranCInterface_EXE)
+ if(FortranCInterface_COMPILED)
+-  find_program(FortranCInterface_EXE
+-    NAMES FortranCInterface${CMAKE_EXECUTABLE_SUFFIX}
+-    PATHS ${FortranCInterface_BINARY_DIR} ${FortranCInterface_BINARY_DIR}/Release
+-    NO_DEFAULT_PATH
+-    )
+-  set(FortranCInterface_EXE ${FortranCInterface_EXE})
+-  unset(FortranCInterface_EXE CACHE)
++  include(${FortranCInterface_BINARY_DIR}/exe-Release.cmake OPTIONAL)
+ else()
+   set(_result "Failed to compile")
+-  set(FortranCInterface_EXE)
+   file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
+     "Fortran/C interface test project failed with the following output:\n"
+     "${FortranCInterface_OUTPUT}\n")

meta-pytorch/recipes-devtools/cmake/cmake-native_%.bbappend
@@ -1,0 +1,3 @@
+FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+
+SRC_URI += "file://0001-FortranCInterface-Fix-broken-search-for-test-exe-whe.patch"

meta-pytorch/recipes-devtools/python/python-future_0.16.0.bb
@@ -1,0 +1,16 @@
+# Recipe from:
+# http://git.openembedded.org/meta-openembedded/tree/meta-python/recipes-devtools/python/python-future_0.16.0.bb
+
+DESCRIPTION = "Clean, single-source support for Python 3 and 2"
+HOMEPAGE = "https://python-future.org"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=3f457538b297bd0ef86564b8ac216ad7"
+
+SRC_URI[md5sum] = "3e8e88a2bda48d54b1da7634d04760d7"
+SRC_URI[sha256sum] = "e39ced1ab767b5936646cedba8bcce582398233d6a627067d4c6a454c90cfedb"
+
+PYPI_PACKAGE_HASH = "99abde815842bc6e97d5a7806ad51236630da14ca2f3b1fce94c0bb94d3d"
+
+inherit pypi setuptools
+
+BBCLASSEXTEND = "native"

meta-pytorch/recipes-devtools/python/python-numpy/0001-ragged-set-of-patches-to-allow-cross-compile.patch
@@ -1,0 +1,67 @@
+From e144c3b86abb49bbc943a17e5271bf880dfca5df Mon Sep 17 00:00:00 2001
+From: Matthias Schoepfer <matthias.schoepfer@identpro.de>
+Date: Thu, 22 Mar 2018 15:57:13 +0100
+Subject: [PATCH] ragged set of patches to allow cross compile
+
+---
+ numpy/distutils/command/build_ext.py | 4 ++--
+ numpy/distutils/exec_command.py      | 1 +
+ numpy/distutils/fcompiler/gnu.py     | 2 +-
+ numpy/version.py                     | 2 +-
+ 4 files changed, 5 insertions(+), 4 deletions(-)
+
+diff --git a/numpy/distutils/command/build_ext.py b/numpy/distutils/command/build_ext.py
+index 0fa52a2..e2154e7 100644
+--- a/numpy/distutils/command/build_ext.py
++++ b/numpy/distutils/command/build_ext.py
+@@ -411,10 +411,10 @@ class build_ext (old_build_ext):
+                                            depends=ext.depends)
+ 
+         objects = c_objects + f_objects
+-
++        from distutils.sysconfig import get_config_vars
+         if ext.extra_objects:
+             objects.extend(ext.extra_objects)
+-        extra_args = ext.extra_link_args or []
++        extra_args = get_config_vars('LDSHARED')[0].split(' ')[1:] + (ext.extra_link_args or [])
+         libraries = self.get_libraries(ext)[:]
+         library_dirs = ext.library_dirs[:]
+ 
+diff --git a/numpy/distutils/exec_command.py b/numpy/distutils/exec_command.py
+index 8faf4b2..d928ada 100644
+--- a/numpy/distutils/exec_command.py
++++ b/numpy/distutils/exec_command.py
+@@ -113,6 +113,7 @@ def find_executable(exe, path=None, _cache={}):
+         fn = os.path.join(path, exe)
+         for s in suffixes:
+             f_ext = fn+s
++            f_ext = f_ext.split(' ')[0]
+             if not os.path.islink(f_ext):
+                 f_ext = realpath(f_ext)
+             if os.path.isfile(f_ext) and os.access(f_ext, os.X_OK):
+diff --git a/numpy/distutils/fcompiler/gnu.py b/numpy/distutils/fcompiler/gnu.py
+index 4649fd7..61cafd6 100644
+--- a/numpy/distutils/fcompiler/gnu.py
++++ b/numpy/distutils/fcompiler/gnu.py
+@@ -261,7 +261,7 @@ class Gnu95FCompiler(GnuFCompiler):
+                     self.executables[key].append('-mno-cygwin')
+         return v
+ 
+-    possible_executables = ['gfortran', 'f95']
++    possible_executables = [os.environ.get('FC', 'gfortran'), 'f95']
+     executables = {
+         'version_cmd'  : ["<F90>", "-dumpversion"],
+         'compiler_f77' : [None, "-Wall", "-g", "-ffixed-form",
+diff --git a/numpy/distutils/system_info.py b/numpy/distutils/system_info.py
+index c1cb5fb..75bbfe2 100644
+--- a/numpy/distutils/system_info.py
++++ b/numpy/distutils/system_info.py
+@@ -620,7 +620,7 @@ class system_info(object):
+         path = self.get_paths(self.section, key)
+         if path == ['']:
+             path = []
+-        return path
++        return []
+ 
+     def get_include_dirs(self, key='include_dirs'):
+         return self.get_paths(self.section, key)

commit bb0742c4ca5bfd1dd2b73dced4a4e50f38b0bccf

pytorch: Add missing hihope-rzg2n support
Fixes: a734dc76e328 ("pytorch: Add PyTorch v1.4.1 support")

Signed-off-by: Chris Paterson <chris.paterson2@renesas.com>

meta-pytorch/recipes-devtools/torchvision/torchvision_0.5.0.bb
@@ -10,7 +10,7 @@ S = "${WORKDIR}/git"
 SRC_URI = "git://github.com/pytorch/vision.git;protocol=git"
 SRCREV = "3bd7a405093742e837b2dc48d3c83fc16614d5df"
 
-COMPATIBLE_MACHINE = "(x86_64|iwg20m-g1m|iwg21m|iwg22m|hihope-rzg2m|ek874)"
+COMPATIBLE_MACHINE = "(x86_64|iwg20m-g1m|iwg21m|iwg22m|hihope-rzg2m|hihope-rzg2n|ek874)"
 
 inherit pythonnative setuptools
 

meta-pytorch/recipes-mathematics/pytorch/pytorch_1.4.1.bb
@@ -22,7 +22,7 @@ SRC_URI_append_arm = " \
 SRC_URI_append_aarch64 = " \
 	file://0002-add-cmake-configuration-for-aarch64.patch \
 "
-COMPATIBLE_MACHINE = "(x86_64|iwg20m-g1m|iwg21m|iwg22m|hihope-rzg2m|ek874)"
+COMPATIBLE_MACHINE = "(x86_64|iwg20m-g1m|iwg21m|iwg22m|hihope-rzg2m|hihope-rzg2n|ek874)"
 
 inherit pythonnative setuptools
 