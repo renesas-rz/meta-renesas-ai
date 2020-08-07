renesas-rz/meta-renesas-ai

commit 10e2ad5dcae6e4362203c614b60e1a8f3c278f04

benchmark: Add Google Coral TPU benchmark support
google-coral-benchmark/label_image application copied from
meta-renesas-ai/meta-google-coral/recipes-mathematics/google-coral/files/label_image

For the provided Resnet models.

Signed-off-by: Chris Paterson <chris.paterson2@renesas.com>

meta-benchmark/recipes-benchmark/google-coral/google-coral_%.bbappend
@@ -1,0 +1,37 @@
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+
+RDEPENDS_${PN} += " \
+	python \
+	python-pip \
+	python-setuptools \
+	python-numpy \
+"
+
+SRC_URI += " \
+	file://google-coral-benchmark \
+	file://labels.txt \
+	file://test_file_list_Resnet.txt \
+	file://run_TPU_measurement.py \
+"
+
+do_compile_append() {
+        ${CXX} -std=c++11 ${S}/../google-coral-benchmark/label_image.cc \
+                ${S}/../google-coral-benchmark/bitmap_helpers.cc \
+                -o ${S}/../google-coral-tpu-benchmark \
+                -I . -I edgetpu -I libedgetpu/  \
+                -I ${STAGING_DIR_TARGET}/usr/include/tensorflow/lite/tools/make/downloads/flatbuffers/include \
+                -lstdc++ -lpthread -lm -ldl ${STAGING_DIR_TARGET}/usr/${TFLITE_LIB_DIR_ARCH}/libtensorflow-lite.a \
+                -l:libedgetpu.so.1.0 -L libedgetpu/${GOOGLE_CORAL_SPEED}/${TPU_LIB_DIR_ARCH} ${LDFLAGS}
+}
+
+do_install_append() {
+	install -d ${D}${bindir}/google-coral-benchmark
+	install -m 0555 ${S}/../google-coral-tpu-benchmark ${D}${bindir}/google-coral-benchmark/
+	install -m 0644 ${S}/../labels.txt ${D}${bindir}/google-coral-benchmark/
+	install -m 0644 ${S}/../test_file_list_Resnet.txt ${D}${bindir}/google-coral-benchmark/
+	install -m 0555 ${S}/../run_TPU_measurement.py ${D}${bindir}/google-coral-benchmark/
+}
+
+FILES_${PN} += "\
+	${bindir}/google-coral-benchmark/* \
+"

meta-benchmark/recipes-benchmark/benchmark/benchmark.bb
@@ -3,7 +3,7 @@ LICENSE = "Apache-2.0"
 LICENSE_FLAGS = "commercial"
 LIC_FILES_CHKSUM = " \
         file://license/COPYING;md5=c4e89413e9e0e6a372520647a3fae1ae \
-        file://license/LICENSES;md5=8d20b58b243160c86661891607a0d385 \
+        file://license/LICENSES;md5=14b082269873137b902e0feca4c4e8f0 \
 "
 
 S = "${WORKDIR}"
@@ -283,6 +283,7 @@ do_install () {
 	install -d ${D}/home/root/models/caffe
 	install -d ${D}/home/root/models/onnx
 	install -d ${D}/home/root/models/darknet
+	install -d ${D}/home/root/models/google-coral/Resnet
 	install -m 0644 ${S}/mobilenet_v1*.tflite ${D}/home/root/models/tensorflowlite/Mobile_Net_V1_Model/
 	install -m 0644 ${S}/mobilenet_v2*.tflite ${D}/home/root/models/tensorflowlite/Mobile_Net_V2_Model/
 	install -m 0644 ${S}/v3*/v3*.tflite ${D}/home/root/models/tensorflowlite/Mobile_Net_V3_Model/
@@ -296,6 +297,7 @@ do_install () {
 	install -m 0644 ${S}/labels.txt ${D}/home/root/models/tensorflowlite/NasNet/
 	install -m 0644 ${S}/labels.txt ${D}/home/root/models/tensorflowlite/Squeezenet/
 	install -m 0644 ${S}/labels.txt ${D}/home/root/models/tensorflowlite/Resnet/
+	install -m 0644 ${S}/labels.txt ${D}/home/root/models/google-coral/Resnet/
 	install -m 0644 ${S}/imagenet_slim_labels.txt ${D}/home/root/models/tensorflow/InceptionV3/
 	install -m 0644 ${S}/imagenet_slim_labels.txt ${D}/home/root/models/tensorflow/InceptionV3_Quant/
 	install -m 0644 ${S}/imagenet_slim_labels.txt ${D}/home/root/models/tensorflowlite/Mobile_InceptionV3/
@@ -314,6 +316,7 @@ do_install () {
 	cp -r ${S}/models/tensorflowlite/* ${D}/home/root/models/tensorflowlite/
 	cp -r ${S}/models/tensorflow/* ${D}/home/root/models/tensorflow/
 	cp -r ${S}/models/armnn/* ${D}/home/root/models/armnn/
+	cp -r ${S}/models/google-coral/* ${D}/home/root/models/google-coral/
 }
 
 FILES_${PN} = " \

meta-benchmark/recipes-benchmark/benchmark/files/license/LICENSES
@@ -449,6 +449,11 @@ Source:Converted from inception_resnet_v2.tflite
 License:Apache License 2.0
 md5sum:bcc2d020c7c96749c1eb3c48dcb596bb
 
+FileName:inception_resnet_v2_quant_edgetpu.tflite
+Source:Converted from inception_resnet_v2_quant.tflite
+License:Apache License 2.0
+md5sum:493fb0f8fb7550f9513d7cab64056320
+
 FileName:resnet_float.tflite
 Source:https://android.googlesource.com/platform/test/mlts/models/+archive/982ff5d52744f8b81583b42a86eefb676dcbbc40.tar.gz
 License:Apache License 2.0
@@ -459,6 +464,11 @@ Source:https://android.googlesource.com/platform/test/mlts/models/+archive/982ff
 License:Apache License 2.0
 md5sum:a0f48909330cefd0a0cdf5ebf2d66656
 
+FileName:resnet_quantized_edgetpu.tflite
+Source:Converted from resnet_quantized.tflite
+License:Apache License 2.0
+md5sum:d1116922e52d63cc687e1a0b29cf4f20
+
 FileName:resnet_v2_101_299.tflite
 Source:https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/resnet_v2_101.tgz
 License:Apache License 2.0
@@ -468,3 +478,8 @@ FileName:resnet_v2_101_299_quant.tflite
 Source:Converted from resnet_v2_101_299.tflite
 License:Apache License 2.0
 md5sum:9f54dfc686b93330f41bb617248a66fe
+
+FileName:resnet_v2_101_299_quant_edgetpu.tflite
+Source:Converted from resnet_v2_101_299_quant.tflite
+License:Apache License 2.0
+md5sum:23aa5e51ba36d30eff5e5b2bd05dd126

meta-benchmark/recipes-benchmark/google-coral/files/google-coral-benchmark/bitmap_helpers.cc
@@ -1,0 +1,120 @@
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+#include <unistd.h>  // NOLINT(build/include_order)
+
+#include "bitmap_helpers.h"
+
+#define LOG(x) std::cerr
+
+namespace tflite {
+namespace label_image {
+
+std::vector<uint8_t> decode_bmp(const uint8_t* input, int row_size, int width,
+                                int height, int channels, bool top_down) {
+  std::vector<uint8_t> output(height * width * channels);
+  for (int i = 0; i < height; i++) {
+    int src_pos;
+    int dst_pos;
+
+    for (int j = 0; j < width; j++) {
+      if (!top_down) {
+        src_pos = ((height - 1 - i) * row_size) + j * channels;
+      } else {
+        src_pos = i * row_size + j * channels;
+      }
+
+      dst_pos = (i * width + j) * channels;
+
+      switch (channels) {
+        case 1:
+          output[dst_pos] = input[src_pos];
+          break;
+        case 3:
+          // BGR -> RGB
+          output[dst_pos] = input[src_pos + 2];
+          output[dst_pos + 1] = input[src_pos + 1];
+          output[dst_pos + 2] = input[src_pos];
+          break;
+        case 4:
+          // BGRA -> RGBA
+          output[dst_pos] = input[src_pos + 2];
+          output[dst_pos + 1] = input[src_pos + 1];
+          output[dst_pos + 2] = input[src_pos];
+          output[dst_pos + 3] = input[src_pos + 3];
+          break;
+        default:
+          LOG(FATAL) << "Unexpected number of channels: " << channels;
+          break;
+      }
+    }
+  }
+  return output;
+}
+
+std::vector<uint8_t> read_bmp(const std::string& input_bmp_name, int* width,
+                              int* height, int* channels, Settings* s) {
+  int begin, end;
+
+  std::ifstream file(input_bmp_name, std::ios::in | std::ios::binary);
+  if (!file) {
+    LOG(FATAL) << "input file " << input_bmp_name << " not found\n";
+    exit(-1);
+  }
+
+  begin = file.tellg();
+  file.seekg(0, std::ios::end);
+  end = file.tellg();
+  size_t len = end - begin;
+
+  if (s->verbose) LOG(INFO) << "len: " << len << "\n";
+
+  std::vector<uint8_t> img_bytes(len);
+  file.seekg(0, std::ios::beg);
+  file.read(reinterpret_cast<char*>(img_bytes.data()), len);
+  const int32_t header_size =
+      *(reinterpret_cast<const int32_t*>(img_bytes.data() + 10));
+  *width = *(reinterpret_cast<const int32_t*>(img_bytes.data() + 18));
+  *height = *(reinterpret_cast<const int32_t*>(img_bytes.data() + 22));
+  const int32_t bpp =
+      *(reinterpret_cast<const int32_t*>(img_bytes.data() + 28));
+  *channels = bpp / 8;
+
+  if (s->verbose)
+    LOG(INFO) << "width, height, channels: " << *width << ", " << *height
+              << ", " << *channels << "\n";
+
+  // there may be padding bytes when the width is not a multiple of 4 bytes
+  // 8 * channels == bits per pixel
+  const int row_size = (8 * *channels * *width + 31) / 32 * 4;
+
+  // if height is negative, data layout is top down
+  // otherwise, it's bottom up
+  bool top_down = (*height < 0);
+
+  // Decode image, allocating tensor once the image size is known
+  const uint8_t* bmp_pixels = &img_bytes[header_size];
+  return decode_bmp(bmp_pixels, row_size, *width, abs(*height), *channels,
+                    top_down);
+}
+
+}  // namespace label_image
+}  // namespace tflite

meta-benchmark/recipes-benchmark/google-coral/files/google-coral-benchmark/bitmap_helpers.h
@@ -1,0 +1,42 @@
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_BITMAP_HELPERS_H_
+#define TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_BITMAP_HELPERS_H_
+
+#include "bitmap_helpers_impl.h"
+#include "label_image.h"
+
+namespace tflite {
+namespace label_image {
+
+std::vector<uint8_t> read_bmp(const std::string& input_bmp_name, int* width,
+                              int* height, int* channels, Settings* s);
+
+template <class T>
+void resize(T* out, uint8_t* in, int image_height, int image_width,
+            int image_channels, int wanted_height, int wanted_width,
+            int wanted_channels, Settings* s);
+
+// explicit instantiation
+template void resize<uint8_t>(uint8_t*, unsigned char*, int, int, int, int, int,
+                              int, Settings*);
+template void resize<float>(float*, unsigned char*, int, int, int, int, int,
+                            int, Settings*);
+
+}  // namespace label_image
+}  // namespace tflite
+
+#endif  // TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_BITMAP_HELPERS_H_

meta-benchmark/recipes-benchmark/google-coral/files/google-coral-benchmark/bitmap_helpers_impl.h
@@ -1,0 +1,95 @@
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_BITMAP_HELPERS_IMPL_H_
+#define TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_BITMAP_HELPERS_IMPL_H_
+
+#include "label_image.h"
+
+#include "tensorflow/lite/builtin_op_data.h"
+#include "tensorflow/lite/interpreter.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/string_util.h"
+
+namespace tflite {
+namespace label_image {
+
+template <class T>
+void resize(T* out, uint8_t* in, int image_height, int image_width,
+            int image_channels, int wanted_height, int wanted_width,
+            int wanted_channels, Settings* s) {
+  int number_of_pixels = image_height * image_width * image_channels;
+  std::unique_ptr<Interpreter> interpreter(new Interpreter);
+
+  int base_index = 0;
+
+  // two inputs: input and new_sizes
+  interpreter->AddTensors(2, &base_index);
+  // one output
+  interpreter->AddTensors(1, &base_index);
+  // set input and output tensors
+  interpreter->SetInputs({0, 1});
+  interpreter->SetOutputs({2});
+
+  // set parameters of tensors
+  TfLiteQuantizationParams quant;
+  interpreter->SetTensorParametersReadWrite(
+      0, kTfLiteFloat32, "input",
+      {1, image_height, image_width, image_channels}, quant);
+  interpreter->SetTensorParametersReadWrite(1, kTfLiteInt32, "new_size", {2},
+                                            quant);
+  interpreter->SetTensorParametersReadWrite(
+      2, kTfLiteFloat32, "output",
+      {1, wanted_height, wanted_width, wanted_channels}, quant);
+
+  ops::builtin::BuiltinOpResolver resolver;
+  const TfLiteRegistration* resize_op =
+      resolver.FindOp(BuiltinOperator_RESIZE_BILINEAR, 1);
+  auto* params = reinterpret_cast<TfLiteResizeBilinearParams*>(
+      malloc(sizeof(TfLiteResizeBilinearParams)));
+  params->align_corners = false;
+  interpreter->AddNodeWithParameters({0, 1}, {2}, nullptr, 0, params, resize_op,
+                                     nullptr);
+
+  interpreter->AllocateTensors();
+
+  // fill input image
+  // in[] are integers, cannot do memcpy() directly
+  auto input = interpreter->typed_tensor<float>(0);
+  for (int i = 0; i < number_of_pixels; i++) {
+    input[i] = in[i];
+  }
+
+  // fill new_sizes
+  interpreter->typed_tensor<int>(1)[0] = wanted_height;
+  interpreter->typed_tensor<int>(1)[1] = wanted_width;
+
+  interpreter->Invoke();
+
+  auto output = interpreter->typed_tensor<float>(2);
+  auto output_number_of_pixels = wanted_height * wanted_width * wanted_channels;
+
+  for (int i = 0; i < output_number_of_pixels; i++) {
+    if (s->input_floating)
+      out[i] = (output[i] - s->input_mean) / s->input_std;
+    else
+      out[i] = (uint8_t)output[i];
+  }
+}
+
+}  // namespace label_image
+}  // namespace tflite
+
+#endif  // TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_BITMAP_HELPERS_IMPL_H_

meta-benchmark/recipes-benchmark/google-coral/files/google-coral-benchmark/get_top_n.h
@@ -1,0 +1,38 @@
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_GET_TOP_N_H_
+#define TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_GET_TOP_N_H_
+
+#include "get_top_n_impl.h"
+
+namespace tflite {
+namespace label_image {
+
+template <class T>
+void get_top_n(T* prediction, int prediction_size, size_t num_results,
+               float threshold, std::vector<std::pair<float, int>>* top_results,
+               bool input_floating);
+
+// explicit instantiation so that we can use them otherwhere
+template void get_top_n<uint8_t>(uint8_t*, int, size_t, float,
+                                 std::vector<std::pair<float, int>>*, bool);
+template void get_top_n<float>(float*, int, size_t, float,
+                               std::vector<std::pair<float, int>>*, bool);
+
+}  // namespace label_image
+}  // namespace tflite
+
+#endif  // TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_GET_TOP_N_H_

meta-benchmark/recipes-benchmark/google-coral/files/google-coral-benchmark/get_top_n_impl.h
@@ -1,0 +1,70 @@
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_GET_TOP_N_IMPL_H_
+#define TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_GET_TOP_N_IMPL_H_
+
+#include <algorithm>
+#include <queue>
+
+namespace tflite {
+namespace label_image {
+
+extern bool input_floating;
+
+// Returns the top N confidence values over threshold in the provided vector,
+// sorted by confidence in descending order.
+template <class T>
+void get_top_n(T* prediction, int prediction_size, size_t num_results,
+               float threshold, std::vector<std::pair<float, int>>* top_results,
+               bool input_floating) {
+  // Will contain top N results in ascending order.
+  std::priority_queue<std::pair<float, int>, std::vector<std::pair<float, int>>,
+                      std::greater<std::pair<float, int>>>
+      top_result_pq;
+
+  const long count = prediction_size;  // NOLINT(runtime/int)
+  for (int i = 0; i < count; ++i) {
+    float value;
+    if (input_floating)
+      value = prediction[i];
+    else
+      value = prediction[i] / 255.0;
+    // Only add it if it beats the threshold and has a chance at being in
+    // the top N.
+    if (value < threshold) {
+      continue;
+    }
+
+    top_result_pq.push(std::pair<float, int>(value, i));
+
+    // If at capacity, kick the smallest value out.
+    if (top_result_pq.size() > num_results) {
+      top_result_pq.pop();
+    }
+  }
+
+  // Copy to output vector and reverse into descending order.
+  while (!top_result_pq.empty()) {
+    top_results->push_back(top_result_pq.top());
+    top_result_pq.pop();
+  }
+  std::reverse(top_results->begin(), top_results->end());
+}
+
+}  // namespace label_image
+}  // namespace tflite
+
+#endif  // TENSORFLOW_LITE_EXAMPLES_LABEL_IMAGE_GET_TOP_N_IMPL_H_

commit 5bf66b518eb8d4cdb342a377cfb5f74feec2bc32

benchmark: google-coral: Add missing mobilenet v2 text file
Fixes: 10e2ad5dcae6 ("benchmark: Add Google Coral TPU benchmark support")

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

meta-benchmark/recipes-benchmark/google-coral/google-coral_%.bbappend
@@ -11,6 +11,7 @@ SRC_URI += " \
 	file://google-coral-benchmark \
 	file://labels.txt \
 	file://test_file_list_Resnet.txt \
+	file://test_file_list_MobileNet_v2.txt \
 	file://run_TPU_measurement.py \
 "
 
@@ -29,6 +30,7 @@ do_install_append() {
 	install -m 0555 ${S}/../google-coral-tpu-benchmark ${D}${bindir}/google-coral-benchmark/
 	install -m 0644 ${S}/../labels.txt ${D}${bindir}/google-coral-benchmark/
 	install -m 0644 ${S}/../test_file_list_Resnet.txt ${D}${bindir}/google-coral-benchmark/
+	install -m 0644 ${S}/../test_file_list_MobileNet_v2.txt ${D}${bindir}/google-coral-benchmark/
 	install -m 0555 ${S}/../run_TPU_measurement.py ${D}${bindir}/google-coral-benchmark/
 }
 