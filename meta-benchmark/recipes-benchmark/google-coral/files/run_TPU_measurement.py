76904a1c179f96e8afc48874bb40e70fb9d25c94
Chris Paterson <chris.paterson2@renesas.com>
2020-11-10 20:12:48 +0000

    run_TPU_measurement: Use tabs for indentations
    Signed-off-by: Chris Paterson <chris.paterson2@renesas.com>

--- meta-benchmark/recipes-benchmark/google-coral/files/run_TPU_measurement.py
+++ meta-benchmark/recipes-benchmark/google-coral/files/run_TPU_measurement.py
@@ -17,70 +17,71 @@ from subprocess import call
 import numpy as np
 
 def main():
-   print("Google Coral TPU Benchmark App")
+	print("Google Coral TPU Benchmark App")
 
-   if len(sys.argv) != 4:
-       print("invalid parameters")
-       print("Example python run_TPU_measurement.py test_file_list_Resnet.txt /home/root/models/google-coral/Resnet 30")
-       sys.exit(1)
+	if len(sys.argv) != 4:
+		print("Invalid parameters")
+		print("Example python run_TPU_measurement.py test_file_list_Resnet.txt /home/root/models/google-coral/Resnet 30")
 
-   filepath = sys.argv[1]
-   if not filepath:
-       print("need to provide model list file")
-       sys.exit(1)
+		sys.exit(1)
 
-   if not os.path.isfile(filepath):
-       print("File path {} does not exist. Exiting...".format(filepath))
-       sys.exit(1)
+	filepath = sys.argv[1]
+	if not filepath:
+		print("Need to provide model list file")
+		sys.exit(1)
 
-   base_directory_path = sys.argv[2]
+	if not os.path.isfile(filepath):
+		print("File path {} does not exist. Exiting...".format(filepath))
+		sys.exit(1)
 
-   number_of_iteration = int(sys.argv[3])
+	base_directory_path = sys.argv[2]
 
-   with open(filepath) as fp:
-       for line in fp:
-	   if not len(line.strip()) == 0:
-	       list = []
-	       list_tmp = []
+	number_of_iteration = int(sys.argv[3])
 
-	       run_label_image(line,base_directory_path,'labels.txt',number_of_iteration,list_tmp,list)
+	with open(filepath) as fp:
+		for line in fp:
+			if not len(line.strip()) == 0:
+				list = []
+				list_tmp = []
 
-	       print("Average Time" + " at Model " + line + str(Average(list_tmp)) + " ms ")
-               print("Standard Deviation" + " at Model " + line + str(Average(list)))
+			run_label_image(line,base_directory_path,'labels.txt',number_of_iteration,list_tmp,list)
 
-               print("\n")
+			print("Average Time" + " at Model " + line + str(Average(list_tmp)) + " ms ")
+			print("Standard Deviation" + " at Model " + line + str(Average(list)))
 
-               if "quant" not in line:
-                   model_type = ",Float,"
-               else:
-                   model_type = ",Quant,"
+			print("\n")
 
-               print("AI_BENCHMARK_MARKER,Google Coral TPU diploria2: TensorFlow Lite," + line.rstrip() + model_type + str(Average(list_tmp)) + "," + str(Average(list)) + ",")
+			if "quant" not in line:
+				model_type = ",Float,"
+			else:
+				model_type = ",Quant,"
+
+			print("AI_BENCHMARK_MARKER,Google Coral TPU diploria2: TensorFlow Lite," + line.rstrip() + model_type + str(Average(list_tmp)) + "," + str(Average(list)) + ",")
 
 def Average(lst):
-    return sum(lst) / len(lst)
+	return sum(lst) / len(lst)
 
 def run_label_image(model_file_name,base_directory,label_file_name,times_to_run,list,list_dev):
-    command = "/usr/bin/google-coral-benchmark/google-coral-tpu-benchmark -i /usr/bin/google-coral/images/grace_hopper_224_224.bmp -c %s -l %s -m %s" % (times_to_run, label_file_name, base_directory+model_file_name)
+	command = "/usr/bin/google-coral-benchmark/google-coral-tpu-benchmark -i /usr/bin/google-coral/images/grace_hopper_224_224.bmp -c %s -l %s -m %s" % (times_to_run, label_file_name, base_directory+model_file_name)
 
-    for line in run_command(command):
-        count = 0
-        if line.find("Average Time") != -1:
-            line = line.split(" ")
-            list.insert(count, float(line[3]))
-            count = count + 1
-        elif line.find("Standard Deviation") != -1:
-            line = line.split(" ")
-            list_dev.insert(count, float(line[2]))
+	for line in run_command(command):
+		count = 0
+		if line.find("Average Time") != -1:
+			line = line.split(" ")
+			list.insert(count, float(line[3]))
+			count = count + 1
+		elif line.find("Standard Deviation") != -1:
+			line = line.split(" ")
+			list_dev.insert(count, float(line[2]))
 
 
 def run_command(command):
-    #Debug
-    #print("Run Command: " + command)
-    p = subprocess.Popen(command,shell=True,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
-    return iter(p.stdout.readline, b'')
+	#Debug
+	#print("Run Command: " + command)
+	p = subprocess.Popen(command,shell=True,
+			     stdout=subprocess.PIPE,
+			     stderr=subprocess.STDOUT)
+	return iter(p.stdout.readline, b'')
 
 if __name__ == '__main__':
-   main()
+	main()
