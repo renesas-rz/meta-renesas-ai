3da1a8414d20e4790a4c615cef3c90d7f8a16f8e
Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
2021-05-12 18:55:47 +0100

    meta-benchmark: caffe2: caffe2_Benchmark: Fail on benchmark error
    Make sure we return error on benchmark failure.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>

--- meta-benchmark/recipes-benchmark/caffe2/files/caffe2_Benchmark.sh
+++ meta-benchmark/recipes-benchmark/caffe2/files/caffe2_Benchmark.sh
@@ -2,6 +2,19 @@
 
 export PYTHONPATH=$PYTHONPATH:/usr
 
+SUCCESS=true
+
 ./caffe2_Benchmark.py squeezenet grace_hopper.jpg
+if [ $? != 0 ]; then
+	SUCCESS=false
+fi
 
 ./caffe2_Benchmark.py mobilenetv2_1.0_224 grace_hopper.jpg
+if [ $? != 0 ]; then
+	SUCCESS=false
+fi
+
+if ! ${SUCCESS}; then
+	>&2 echo "ERROR: One or more tests have failed."
+	exit 2
+fi
