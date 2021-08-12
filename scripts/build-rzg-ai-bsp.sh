renesas-rz/meta-renesas-ai

commit 7a858eda3207cfc8ae10ade90a5feafcc45068df

meta-tensorflow: Remove support
Remove support for the tensorflow framework.

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

scripts/build-rzg-ai-bsp.sh
@@ -19,7 +19,7 @@ WORK_DIR="${PWD}"
 COMMAND_NAME="$0"
 INSTALL_DEPENDENCIES=false
 PLATFORM=""
-FRAMEWORK="benchmark-a"
+FRAMEWORK="benchmark-armnn+tfl"
 BENCHMARK=true
 PROP_DIR=""
 BUILD=true
@@ -47,12 +47,11 @@ print_help () {
 	 -f <framework>     Select which AI framework to include in the
 	                    filesystem.
 	                    Choose from:
-	                    armnn, benchmark-a, benchmark-b,
-	                    onnxruntime, tensorflow,
-	                    or tensorflow-lite.
-	                    The benchmark-a option will automatically include
-	                    armnn, tensorflow, and tensorflow-lite.
-	                    The benchmark-b option will automatically include
+	                    benchmark-armnn, benchmark-tflite, armnn,
+	                    onnxruntime or tensorflow-lite.
+	                    The benchmark-armnn+tfl option will automatically include
+	                    armnn and tensorflow-lite.
+	                    The benchmark-onnx option will automatically include
 	                    onnxruntime.
 	                    By default ${FRAMEWORK} will be used.
 	 -l <prop lib dir>  Location when proprietary libraries have been
@@ -79,17 +78,17 @@ while getopts ":cdf:l:o:p:h" opt; do
         f)
 		case "${OPTARG}" in
 		"armnn" | "onnxruntime" | \
-		"tensorflow" | "tensorflow-lite")
+		"tensorflow-lite")
 			FRAMEWORK="${OPTARG}"
 			BENCHMARK=false
         	        ;;
 
-		"benchmark-a")
-			FRAMEWORK="armnn+tf+tfl"
+		"benchmark-armnn+tfl")
+			FRAMEWORK="armnn+tfl"
 			BENCHMARK=true
 			;;
 
-		"benchmark-b")
+		"benchmark-onnx")
 			FRAMEWORK="onnx"
 			BENCHMARK=true
 			;;

meta-armnn/templates/smarc-rzg2l/local.conf
@@ -294,7 +294,7 @@ IMAGE_INSTALL_append = " armnn-dev armnn-examples \
 "
 
 # Mask bbappends that have no recipe from the meta-benchmark layer to prevent errors
-BBMASK_non_debian += "onnxruntime/|tensorflow/"
+BBMASK_non_debian += "onnxruntime/"
 
 # Set library files to populate under /lib64
 BASELIB="lib64"

meta-tensorflow/conf/layer.conf
@@ -1,17 +1,0 @@
-#We have a conf and classes directory, append to BBPATH
-BBPATH .= ":${LAYERDIR}"
-
-# We have a recipes directory, add to BBFILES
-BBFILES += " \
-        ${LAYERDIR}/recipes-*/*/*.bb \
-        ${LAYERDIR}/recipes-*/*/*.bbappend \
-"
-
-BBFILE_COLLECTIONS += "tensorflow-layer"
-BBFILE_PATTERN_tensorflow-layer := "^${LAYERDIR}/"
-BBFILE_PRIORITY_tensorflow-layer = "8"
-
-PREFERRED_VERSION_openjdk-8-native ?= "151-1.b12"
-PREFERRED_VERSION_bazel-native ?= "3.5.0"
-PREFERRED_VERSION_tensorflow ?= "2.3.1%"
-PREFERRED_VERSION_libeigen ?= "3.3.2"

meta-tensorflow/recipes-devtools/bazel/bazel-native.inc
@@ -1,30 +1,0 @@
-DESCRIPTION = "Bazel build and test tool"
-
-SRC_URI = "https://github.com/bazelbuild/bazel/releases/download/${PV}/bazel-${PV}-dist.zip"
-
-inherit native
-
-S = "${WORKDIR}"
-
-INHIBIT_SYSROOT_STRIP = "1"
-
-DEPENDS = "coreutils-native zip-native"
-
-do_compile () {
-        export HTTP_PROXY=${HTTP_PROXY}
-        export HTTPS_PROXY=${HTTPS_PROXY}
-        export http_proxy=${HTTP_PROXY}
-        export https_proxy=${HTTPS_PROXY}
-        export JAVA_HOME="${bindir}/openjdk-1.8-native/"
-        ./compile.sh
-}
-
-do_install () {
-        install -d ${D}${bindir}
-        cp -Rf \
-		${S}/output/* \
-		${D}${bindir}
-}
-
-DEPENDS += "openjdk-8-native"
-RDEPENDS_${PN}-native += "openjdk-8-native"

meta-tensorflow/recipes-devtools/bazel/bazel-native_3.5.0.bb
@@ -1,7 +1,0 @@
-LICENSE = "Apache-2.0"
-LIC_FILES_CHKSUM = "file://LICENSE;md5=3b83ef96387f14655fc854ddc3c6bd57"
-
-SRC_URI[md5sum] = "38b0fb11bb22b676338898ed87ec19e7"
-SRC_URI[sha256sum] = "334429059cf82e222ca8a9d9dbbd26f8e1eb308613463c2b8655dd4201b127ec"
-
-include bazel-native.inc

meta-tensorflow/recipes-devtools/openjdk/openjdk-8-native_151-1.b12.bb
@@ -1,28 +1,0 @@
-DESCRIPTION = "Java runtime based upon the OpenJDK Project"
-LICENSE = "GPL-2.0-with-classpath-exception"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/${LICENSE};md5=6133e6794362eff6641708cfcc075b80"
-
-inherit native
-
-INHIBIT_SYSROOT_STRIP = "1"
-
-SRC_URI[md5sum] = "5573dd4caaebce2167ef4c8b7889915a"
-SRC_URI[sha256sum] = "5dccd3d05f489cd6dc5fe645e5f71b3bd29c9f2f8cfac66c6f4d95bba362e639"
-SRC_URI = " \
-	https://github.com/ojdkbuild/ojdkbuild/releases/download/1.8.0.151-1/java-1.8.0-openjdk-1.8.0.151-1.b12.el6_9.x86_64.zip \
-"
-
-S = "${WORKDIR}/java-1.8.0-openjdk-1.8.0.151-1.b12.el6_9.x86_64"
-
-do_install () {
-	install -d ${D}${bindir}/openjdk-1.8-native
-	cp -Rf \
-		${S}/* \
-		${D}${bindir}/openjdk-1.8-native
-
-	cd ${D}${bindir}
-	ln -sf openjdk-1.8-native openjdk-native
-	for TOOL in $(cd openjdk-native/bin; ls); do
-		ln -sf openjdk-native/bin/${TOOL} ${TOOL}
-	done
-}

meta-tensorflow/recipes-mathematics/libeigen/libeigen-3.3.2/0001-CMakeLists.txt-install-FindEigen3.cmake-script.patch
@@ -1,28 +1,0 @@
-From fd8bc66efac1059c1068f804746b01b90177c333 Mon Sep 17 00:00:00 2001
-From: Lukas Bulwahn <lukas.bulwahn@oss.bmw-carit.de>
-Date: Sun, 18 Oct 2015 16:52:25 +0200
-Subject: [PATCH] CMakeLists.txt: install FindEigen3.cmake script
-
-Upstream-Status: Pending
-
-Signed-off-by: Lukas Bulwahn <lukas.bulwahn@oss.bmw-carit.de>
----
- CMakeLists.txt | 2 ++
- 1 file changed, 2 insertions(+)
-
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 76a11b9..ec6c6ea 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -342,6 +342,8 @@ if(EIGEN_BUILD_PKGCONFIG)
-         )
- endif(EIGEN_BUILD_PKGCONFIG)
- 
-+install(FILES "${PROJECT_SOURCE_DIR}/cmake/FindEigen3.cmake" DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cmake/Modules")
-+
- add_subdirectory(Eigen)
- 
- add_subdirectory(doc EXCLUDE_FROM_ALL)
--- 
-1.9.3
-

meta-tensorflow/recipes-mathematics/libeigen/libeigen-3.3.2/0002-neon-jacobiRotation.patch
@@ -1,245 +1,0 @@
-# HG changeset patch
-# User Gael Guennebaud <g.gael@free.fr>
-# Date 1497514590 -7200
-# Node ID d781c1de98342c5ca29c2fe719d8d3c96a35dcd4
-# Parent  48cd83b2b459aa9f3f5dca135d38760fe0b02a2f
-Bug 1436: fix compilation of Jacobi rotations with ARM NEON, some specializations of internal::conj_helper were missing.
-
-diff --git a/Eigen/Core b/Eigen/Core
---- a/Eigen/Core
-+++ b/Eigen/Core
-@@ -371,6 +371,7 @@
- #include "src/Core/MathFunctions.h"
- #include "src/Core/GenericPacketMath.h"
- #include "src/Core/MathFunctionsImpl.h"
-+#include "src/Core/arch/Default/ConjHelper.h"
- 
- #if defined EIGEN_VECTORIZE_AVX512
-   #include "src/Core/arch/SSE/PacketMath.h"
-diff --git a/Eigen/src/Core/arch/AVX/Complex.h b/Eigen/src/Core/arch/AVX/Complex.h
---- a/Eigen/src/Core/arch/AVX/Complex.h
-+++ b/Eigen/src/Core/arch/AVX/Complex.h
-@@ -204,23 +204,7 @@
-   }
- };
- 
--template<> struct conj_helper<Packet8f, Packet4cf, false,false>
--{
--  EIGEN_STRONG_INLINE Packet4cf pmadd(const Packet8f& x, const Packet4cf& y, const Packet4cf& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet4cf pmul(const Packet8f& x, const Packet4cf& y) const
--  { return Packet4cf(Eigen::internal::pmul(x, y.v)); }
--};
--
--template<> struct conj_helper<Packet4cf, Packet8f, false,false>
--{
--  EIGEN_STRONG_INLINE Packet4cf pmadd(const Packet4cf& x, const Packet8f& y, const Packet4cf& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet4cf pmul(const Packet4cf& x, const Packet8f& y) const
--  { return Packet4cf(Eigen::internal::pmul(x.v, y)); }
--};
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet4cf,Packet8f)
- 
- template<> EIGEN_STRONG_INLINE Packet4cf pdiv<Packet4cf>(const Packet4cf& a, const Packet4cf& b)
- {
-@@ -400,23 +384,7 @@
-   }
- };
- 
--template<> struct conj_helper<Packet4d, Packet2cd, false,false>
--{
--  EIGEN_STRONG_INLINE Packet2cd pmadd(const Packet4d& x, const Packet2cd& y, const Packet2cd& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet2cd pmul(const Packet4d& x, const Packet2cd& y) const
--  { return Packet2cd(Eigen::internal::pmul(x, y.v)); }
--};
--
--template<> struct conj_helper<Packet2cd, Packet4d, false,false>
--{
--  EIGEN_STRONG_INLINE Packet2cd pmadd(const Packet2cd& x, const Packet4d& y, const Packet2cd& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet2cd pmul(const Packet2cd& x, const Packet4d& y) const
--  { return Packet2cd(Eigen::internal::pmul(x.v, y)); }
--};
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet2cd,Packet4d)
- 
- template<> EIGEN_STRONG_INLINE Packet2cd pdiv<Packet2cd>(const Packet2cd& a, const Packet2cd& b)
- {
-diff --git a/Eigen/src/Core/arch/AltiVec/Complex.h b/Eigen/src/Core/arch/AltiVec/Complex.h
---- a/Eigen/src/Core/arch/AltiVec/Complex.h
-+++ b/Eigen/src/Core/arch/AltiVec/Complex.h
-@@ -224,23 +224,7 @@
-   }
- };
- 
--template<> struct conj_helper<Packet4f, Packet2cf, false,false>
--{
--  EIGEN_STRONG_INLINE Packet2cf pmadd(const Packet4f& x, const Packet2cf& y, const Packet2cf& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet2cf pmul(const Packet4f& x, const Packet2cf& y) const
--  { return Packet2cf(internal::pmul<Packet4f>(x, y.v)); }
--};
--
--template<> struct conj_helper<Packet2cf, Packet4f, false,false>
--{
--  EIGEN_STRONG_INLINE Packet2cf pmadd(const Packet2cf& x, const Packet4f& y, const Packet2cf& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet2cf pmul(const Packet2cf& x, const Packet4f& y) const
--  { return Packet2cf(internal::pmul<Packet4f>(x.v, y)); }
--};
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet2cf,Packet4f)
- 
- template<> EIGEN_STRONG_INLINE Packet2cf pdiv<Packet2cf>(const Packet2cf& a, const Packet2cf& b)
- {
-@@ -416,23 +400,8 @@
-     return pconj(internal::pmul(a, b));
-   }
- };
--template<> struct conj_helper<Packet2d, Packet1cd, false,false>
--{
--  EIGEN_STRONG_INLINE Packet1cd pmadd(const Packet2d& x, const Packet1cd& y, const Packet1cd& c) const
--  { return padd(c, pmul(x,y)); }
- 
--  EIGEN_STRONG_INLINE Packet1cd pmul(const Packet2d& x, const Packet1cd& y) const
--  { return Packet1cd(internal::pmul<Packet2d>(x, y.v)); }
--};
--
--template<> struct conj_helper<Packet1cd, Packet2d, false,false>
--{
--  EIGEN_STRONG_INLINE Packet1cd pmadd(const Packet1cd& x, const Packet2d& y, const Packet1cd& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet1cd pmul(const Packet1cd& x, const Packet2d& y) const
--  { return Packet1cd(internal::pmul<Packet2d>(x.v, y)); }
--};
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet1cd,Packet2d)
- 
- template<> EIGEN_STRONG_INLINE Packet1cd pdiv<Packet1cd>(const Packet1cd& a, const Packet1cd& b)
- {
-diff --git a/Eigen/src/Core/arch/Default/ConjHelper.h b/Eigen/src/Core/arch/Default/ConjHelper.h
-new file mode 100644
---- /dev/null
-+++ b/Eigen/src/Core/arch/Default/ConjHelper.h
-@@ -0,0 +1,29 @@
-+
-+// This file is part of Eigen, a lightweight C++ template library
-+// for linear algebra.
-+//
-+// Copyright (C) 2017 Gael Guennebaud <gael.guennebaud@inria.fr>
-+//
-+// This Source Code Form is subject to the terms of the Mozilla
-+// Public License v. 2.0. If a copy of the MPL was not distributed
-+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-+
-+#ifndef EIGEN_ARCH_CONJ_HELPER_H
-+#define EIGEN_ARCH_CONJ_HELPER_H
-+
-+#define EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(PACKET_CPLX, PACKET_REAL)                                                          \
-+  template<> struct conj_helper<PACKET_REAL, PACKET_CPLX, false,false> {                                          \
-+    EIGEN_STRONG_INLINE PACKET_CPLX pmadd(const PACKET_REAL& x, const PACKET_CPLX& y, const PACKET_CPLX& c) const \
-+    { return padd(c, pmul(x,y)); }                                                                                \
-+    EIGEN_STRONG_INLINE PACKET_CPLX pmul(const PACKET_REAL& x, const PACKET_CPLX& y) const                        \
-+    { return PACKET_CPLX(Eigen::internal::pmul<PACKET_REAL>(x, y.v)); }                                           \
-+  };                                                                                                              \
-+                                                                                                                  \
-+  template<> struct conj_helper<PACKET_CPLX, PACKET_REAL, false,false> {                                          \
-+    EIGEN_STRONG_INLINE PACKET_CPLX pmadd(const PACKET_CPLX& x, const PACKET_REAL& y, const PACKET_CPLX& c) const \
-+    { return padd(c, pmul(x,y)); }                                                                                \
-+    EIGEN_STRONG_INLINE PACKET_CPLX pmul(const PACKET_CPLX& x, const PACKET_REAL& y) const                        \
-+    { return PACKET_CPLX(Eigen::internal::pmul<PACKET_REAL>(x.v, y)); }                                           \
-+  };
-+
-+#endif // EIGEN_ARCH_CONJ_HELPER_H
-diff --git a/Eigen/src/Core/arch/NEON/Complex.h b/Eigen/src/Core/arch/NEON/Complex.h
---- a/Eigen/src/Core/arch/NEON/Complex.h
-+++ b/Eigen/src/Core/arch/NEON/Complex.h
-@@ -265,6 +265,8 @@
-   }
- };
- 
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet2cf,Packet4f)
-+
- template<> EIGEN_STRONG_INLINE Packet2cf pdiv<Packet2cf>(const Packet2cf& a, const Packet2cf& b)
- {
-   // TODO optimize it for NEON
-@@ -456,6 +458,8 @@
-   }
- };
- 
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet1cd,Packet2d)
-+
- template<> EIGEN_STRONG_INLINE Packet1cd pdiv<Packet1cd>(const Packet1cd& a, const Packet1cd& b)
- {
-   // TODO optimize it for NEON
-diff --git a/Eigen/src/Core/arch/SSE/Complex.h b/Eigen/src/Core/arch/SSE/Complex.h
---- a/Eigen/src/Core/arch/SSE/Complex.h
-+++ b/Eigen/src/Core/arch/SSE/Complex.h
-@@ -229,23 +229,7 @@
-   }
- };
- 
--template<> struct conj_helper<Packet4f, Packet2cf, false,false>
--{
--  EIGEN_STRONG_INLINE Packet2cf pmadd(const Packet4f& x, const Packet2cf& y, const Packet2cf& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet2cf pmul(const Packet4f& x, const Packet2cf& y) const
--  { return Packet2cf(Eigen::internal::pmul<Packet4f>(x, y.v)); }
--};
--
--template<> struct conj_helper<Packet2cf, Packet4f, false,false>
--{
--  EIGEN_STRONG_INLINE Packet2cf pmadd(const Packet2cf& x, const Packet4f& y, const Packet2cf& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet2cf pmul(const Packet2cf& x, const Packet4f& y) const
--  { return Packet2cf(Eigen::internal::pmul<Packet4f>(x.v, y)); }
--};
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet2cf,Packet4f)
- 
- template<> EIGEN_STRONG_INLINE Packet2cf pdiv<Packet2cf>(const Packet2cf& a, const Packet2cf& b)
- {
-@@ -430,23 +414,7 @@
-   }
- };
- 
--template<> struct conj_helper<Packet2d, Packet1cd, false,false>
--{
--  EIGEN_STRONG_INLINE Packet1cd pmadd(const Packet2d& x, const Packet1cd& y, const Packet1cd& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet1cd pmul(const Packet2d& x, const Packet1cd& y) const
--  { return Packet1cd(Eigen::internal::pmul<Packet2d>(x, y.v)); }
--};
--
--template<> struct conj_helper<Packet1cd, Packet2d, false,false>
--{
--  EIGEN_STRONG_INLINE Packet1cd pmadd(const Packet1cd& x, const Packet2d& y, const Packet1cd& c) const
--  { return padd(c, pmul(x,y)); }
--
--  EIGEN_STRONG_INLINE Packet1cd pmul(const Packet1cd& x, const Packet2d& y) const
--  { return Packet1cd(Eigen::internal::pmul<Packet2d>(x.v, y)); }
--};
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet1cd,Packet2d)
- 
- template<> EIGEN_STRONG_INLINE Packet1cd pdiv<Packet1cd>(const Packet1cd& a, const Packet1cd& b)
- {
-diff --git a/Eigen/src/Core/arch/ZVector/Complex.h b/Eigen/src/Core/arch/ZVector/Complex.h
---- a/Eigen/src/Core/arch/ZVector/Complex.h
-+++ b/Eigen/src/Core/arch/ZVector/Complex.h
-@@ -336,6 +336,9 @@
-   }
- };
- 
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet2cf,Packet4f)
-+EIGEN_MAKE_CONJ_HELPER_CPLX_REAL(Packet1cd,Packet2d)
-+
- template<> EIGEN_STRONG_INLINE Packet1cd pdiv<Packet1cd>(const Packet1cd& a, const Packet1cd& b)
- {
-   // TODO optimize it for AltiVec

commit a000d6637dabcabcc6c84b27f2391a60662bc335

build-rzg-ai-bsp.sh: Fix build script
A couple of fixes to the example build script:

1) Fix so that if the default FRAMEWORK setting is used there isn't an
   error when copying the configuration files.
   Fixes: 7a858eda3207 ("meta-tensorflow: Remove support")

2) Close off licensing prompt if statement correctly and simplify read
   command.
   Fixes: 8a0ff5e7489f ("build-rzg-ai-bsp.sh: Prompt user for licensing confirmation")

Signed-off-by: Chris Paterson <chris.paterson2@renesas.com>

scripts/build-rzg-ai-bsp.sh
@@ -75,24 +75,9 @@ while getopts ":cdf:l:o:p:h" opt; do
                 ;;
         f)
 		case "${OPTARG}" in
-		"armnn" | "onnxruntime" | "tensorflow-lite")
+		"armnn" | "onnxruntime" | "tensorflow-lite" | \
+		"benchmark-armnn+tfl" | "benchmark-onnx" | "benchmark-tflite")
 			FRAMEWORK="${OPTARG}"
-			BENCHMARK=false
-        	        ;;
-
-		"benchmark-armnn+tfl")
-			FRAMEWORK="armnn+tfl"
-			BENCHMARK=true
-			;;
-
-		"benchmark-onnx")
-			FRAMEWORK="onnx"
-			BENCHMARK=true
-			;;
-
-		"benchmark-tflite")
-			FRAMEWORK="tflite"
-			BENCHMARK=true
 			;;
 
 		*)
@@ -344,6 +329,28 @@ configure_build () {
 	# This will create and take us to the $WORK_DIR/build directory
 	source poky/oe-init-build-env
 
+	# Remove benchmark from framework name
+	case "${FRAMEWORK}" in
+		"armnn" | "onnxruntime" | "tensorflow-lite")
+			BENCHMARK=false
+			;;
+
+		"benchmark-armnn+tfl")
+			FRAMEWORK="armnn+tfl"
+			BENCHMARK=true
+			;;
+
+		"benchmark-onnx")
+			FRAMEWORK="onnx"
+			BENCHMARK=true
+			;;
+
+		"benchmark-tflite")
+			FRAMEWORK="tflite"
+			BENCHMARK=true
+			;;
+	esac
+
 	if ${BENCHMARK}; then
 		cp $WORK_DIR/meta-renesas-ai/meta-benchmark/templates/${FRAMEWORK}/${PLATFORM}/*.conf ./conf/
 	else
@@ -420,13 +427,14 @@ install_prop_libs
 configure_build
 
 if $BUILD; then
-	read -p "Have licensing options been updated in the local.conf file?\n" -n 1 -r
+	echo -ne "\nHave licensing options been updated in the local.conf file? "; read
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
 		do_build
 		copy_output
 	else
-		echo "Please uncomment the LICENSE_FLAGS_WHITELIST to build the BSP\n"
+		echo "Please uncomment the LICENSE_FLAGS_WHITELIST to build the BSP"
+	fi
 fi
 
 echo "#################################################################"