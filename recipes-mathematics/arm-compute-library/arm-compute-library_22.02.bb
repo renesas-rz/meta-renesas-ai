renesas-rz/meta-renesas-ai

commit feb00d9405f66b4c510accd22d7248916ac9e8aa

arm-compute-library: Set correct ARCH for G2L platforms
Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

recipes-mathematics/arm-compute-library/arm-compute-library_22.02.bb
@@ -33,8 +33,8 @@ OESCONS_COMMON_FLAG = " extra_cxx_flags="${TOOLCHAIN_OPTIONS} -Wno-error=noexcep
 EXTRA_OESCONS_aarch64 = "arch=arm64-v8a${OESCONS_COMMON_FLAG}"
 
 # Override the platform generic opencl flag for RZ/G2L and RZ/G2LC
-OESCONS_COMMON_FLAG_append_smarc-rzg2l  = " opencl=1 embed_kernels=1"
-OESCONS_COMMON_FLAG_append_smarc-rzg2lc = " opencl=1 embed_kernels=1"
+OESCONS_COMMON_FLAG_append_smarc-rzg2l  = " opencl=1 embed_kernels=1 arch=armv8.2-a"
+OESCONS_COMMON_FLAG_append_smarc-rzg2lc = " opencl=1 embed_kernels=1 arch=armv8.2-a"
 
 do_install() {
 	CP_ARGS="-Prf --preserve=mode,timestamps --no-preserve=ownership"

commit 9c8fca86480511887c21f0ce6e0743f86a4c213a

arm-compute-library: Set correct arch for smarc-rzg2ul
Up until now arm64-v8a was being used for smarc-rzg2ul.
Start using armv8.2-a instead so we can get the best out of the Arm
Compute Library.

Fixes: feb00d9405f6 ("arm-compute-library: Set correct ARCH for G2L platforms")

Signed-off-by: Chris Paterson <chris.paterson2@renesas.com>

recipes-mathematics/arm-compute-library/arm-compute-library_22.02.bb
@@ -36,6 +36,7 @@ EXTRA_OESCONS_aarch64 = "arch=arm64-v8a${OESCONS_COMMON_FLAG}"
 # Override the platform generic opencl flag for RZ/G2L, RZ/G2LC and RZ/V2L
 OESCONS_COMMON_FLAG_append_smarc-rzg2l  = " opencl=1 embed_kernels=1 arch=armv8.2-a"
 OESCONS_COMMON_FLAG_append_smarc-rzg2lc = " opencl=1 embed_kernels=1 arch=armv8.2-a"
+OESCONS_COMMON_FLAG_append_smarc-rzg2ul  = " arch=armv8.2-a"
 OESCONS_COMMON_FLAG_append_smarc-rzv2l = " opencl=1 embed_kernels=1 arch=armv8.2-a"
 
 do_install() {